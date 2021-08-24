73a1df8e9fa2aca6d77fdf34b59b1a1bb5201153
kidmin <kidmin+github@gmail.com>
2021-08-25 00:50:31 +0900

    experimental implementation of 'output-encoding'

--- options.c
+++ options.c
@@ -1156,6 +1156,10 @@ options_push_changes(const char *name)
 		RB_FOREACH(wp, window_pane_tree, &all_window_panes)
 			wp->flags |= PANE_STYLECHANGED;
 	}
+	if (strcmp(name, "output-encoding") == 0) {
+		RB_FOREACH(wp, window_pane_tree, &all_window_panes)
+			input_encoding_updated(wp);
+	}
 	if (strcmp(name, "pane-colours") == 0) {
 		RB_FOREACH(wp, window_pane_tree, &all_window_panes)
 			colour_palette_from_option(&wp->palette, wp->options);
